18e119db832f205a95bb8c5adf9c64105e06c2e9
MinarikTomas <tminarik20@gmail.com>
2023-10-27 16:24:15 +0200

    added add card fragment

---
+++ app/src/main/java/sk/stuba/fei/uim/dp/attendance/fragments/AddCardFragment.kt
@@ -0,0 +1,12 @@
+package sk.stuba.fei.uim.dp.attendance.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import sk.stuba.fei.uim.dp.attendance.R
+
+class AddCardFragment : Fragment(R.layout.fragment_add_card) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
