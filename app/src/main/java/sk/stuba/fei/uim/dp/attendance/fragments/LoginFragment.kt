6e5988d3f532a441a8fc4c4418270894b720ad17
TomasMinarik <tminarik20@gmail.com>
2023-10-27 10:50:56 +0200

    added login fragment

---
+++ app/src/main/java/sk/stuba/fei/uim/dp/attendance/fragments/LoginFragment.kt
@@ -0,0 +1,12 @@
+package sk.stuba.fei.uim.dp.attendance.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import sk.stuba.fei.uim.dp.attendance.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
