acdbe65c7ee33673186d98126ac98215602d4770
MinarikTomas <tminarik20@gmail.com>
2023-10-30 15:31:42 +0100

    upload

---
+++ app/src/main/java/sk/stuba/fei/uim/dp/attendance/adapters/ActivitiesAdapter.kt
@@ -0,0 +1,66 @@
+package sk.stuba.fei.uim.dp.attendance.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import sk.stuba.fei.uim.dp.attendance.R
+
+data class MyItem(val theView: Int, val title: String, val location: String, val time: String, val date: String)
+class ActivitiesAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        const val THE_DATE_VIEW = 1
+        const val THE_ACTIVITY_VIEW = 2
+    }
+
+    private var items: List<MyItem> = listOf()
+
+    private inner class DateViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bind(position: Int){
+            itemView.findViewById<TextView>(R.id.item_date).text = items[position].date
+        }
+    }
+
+    private inner class ActivityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bind(position: Int){
+            itemView.findViewById<TextView>(R.id.item_title).text = items[position].title
+            itemView.findViewById<TextView>(R.id.item_location).text = items[position].location
+            itemView.findViewById<TextView>(R.id.item_time).text = items[position].time
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if(viewType == THE_DATE_VIEW){
+            return DateViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.date_item, parent, false)
+            )
+        }
+        return ActivityViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.activity_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(items[position].theView == THE_DATE_VIEW){
+            (holder as DateViewHolder).bind(position)
+        }else {
+            (holder as ActivityViewHolder).bind(position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return items[position].theView
+    }
+
+    fun updateItems(newItems: List<MyItem>){
+        items = newItems
+        notifyDataSetChanged()
+    }
+
+}
--- app/src/main/java/sk/stuba/fei/uim/dp/attendance/fragments/HomeFragment.kt
+++ app/src/main/java/sk/stuba/fei/uim/dp/attendance/fragments/HomeFragment.kt
@@ -3,11 +3,50 @@ package sk.stuba.fei.uim.dp.attendance.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import sk.stuba.fei.uim.dp.attendance.R
+import sk.stuba.fei.uim.dp.attendance.adapters.ActivitiesAdapter
+import sk.stuba.fei.uim.dp.attendance.adapters.MyItem
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val dataList = ArrayList<MyItem>()
+        dataList.add(MyItem(ActivitiesAdapter.THE_DATE_VIEW, date = "30.10.2024", location = "", time = "", title = ""))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "30.10.2024", location = "D104", title = "Functional Programming", time = "10:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "30.10.2024", location = "AB300", title = "Team Project 1", time = "12:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_DATE_VIEW, date = "30.10.2024", location = "", time = "", title = ""))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "31.10.2024", location = "AB300", title = "Network encryption", time = "12:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_DATE_VIEW, date = "1.11.2024", location = "", time = "", title = ""))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "1.11.2024", location = "AB300", title = "Functional Programming", time = "10:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "1.11.2024", location = "AB300", title = "Team Project 1", time = "12:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_DATE_VIEW, date = "30.10.2024", location = "", time = "", title = ""))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "30.10.2024", location = "D104", title = "Functional Programming", time = "10:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "30.10.2024", location = "AB300", title = "Team Project 1", time = "12:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_DATE_VIEW, date = "30.10.2024", location = "", time = "", title = ""))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "31.10.2024", location = "AB300", title = "Network encryption", time = "12:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_DATE_VIEW, date = "1.11.2024", location = "", time = "", title = ""))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "1.11.2024", location = "AB300", title = "Functional Programming", time = "10:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "1.11.2024", location = "AB300", title = "Team Project 1", time = "12:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_DATE_VIEW, date = "30.10.2024", location = "", time = "", title = ""))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "30.10.2024", location = "D104", title = "Functional Programming", time = "10:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "30.10.2024", location = "AB300", title = "Team Project 1", time = "12:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_DATE_VIEW, date = "30.10.2024", location = "", time = "", title = ""))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "31.10.2024", location = "AB300", title = "Network encryption", time = "12:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_DATE_VIEW, date = "1.11.2024", location = "", time = "", title = ""))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "1.11.2024", location = "AB300", title = "Functional Programming", time = "10:00"))
+        dataList.add(MyItem(ActivitiesAdapter.THE_ACTIVITY_VIEW, date = "1.11.2024", location = "AB300", title = "Team Project 1", time = "12:00"))
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview_activities)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        val activitiesAdapter = ActivitiesAdapter()
+        recyclerView.adapter = activitiesAdapter
+        activitiesAdapter.updateItems(dataList)
+
     }
+
+
 }
